b5b8b970a6bead812d9814dcf3445c8305a4df6c
viniciusmiiller <viniciusmiiller>
2017-04-12 20:10:59 -0300

    falaaaaaaaaaaaa

---
+++ programacao3/tarefa3/script.js
@@ -0,0 +1,51 @@
+var dataUrl = "data.json",
+    itensHtml = "item-snippet.html";
+    
+// função facilitadora para inserir HTML em um elemento
+function insereHtml(seletor, html) {
+  var elemento = document.querySelector(seletor);
+  console.log(html);
+  elemento.innerHTML = html;
+}
+
+// substitui propriedade {{propName}} dentro de um 
+// 'template', e substitui por seu propValue
+function inserePropriedade(template, propName, propValue) {
+  // criar {{propName}}
+  // trocar (replace), dentro de template, {{propName}} por propValue
+  // retornar o template alterado
+  var propriedade = "{{" + propName + "}}";
+  // substitui todas as ocorrências de propriedade por propValue
+  // em template
+  template = template.replace(new RegExp(propriedade, "g"),
+              propValue);
+  return template;
+}
+
+// constroi a pagina, com os dados recebidos por parametro
+function constroiPagina(dados) {
+  var htmlFinal = '<section class="row">'; // string que vai conter todo o HTML
+  // construimos os itens agora
+  $ajaxUtils.sendGetRequest(itensHtml, function(itensHtml) {
+    for (var i = 0, max = dados.length; i < max; i++) {
+      var html = itensHtml,
+          nome = dados[i].name.first + " " + dados[i].name.last,
+          empresa = dados[i].company,
+          email = dados[i].email,
+          fone = dados[i].phone;
+          
+      html = inserePropriedade(html, "nome", nome);
+      html = inserePropriedade(html, "empresa", empresa);
+      html = inserePropriedade(html, "email", email);
+      html = inserePropriedade(html, "fone", fone);
+      
+      htmlFinal += html;
+    }
+    htmlFinal += '</section>';
+    insereHtml("#content", htmlFinal);
+  }, false); // não é um JSON
+}
+// vamos construir o sendGetRequest:
+// definir a URL (dataUrl)
+// e o metodo constroiPagina
+$ajaxUtils.sendGetRequest(dataUrl, constroiPagina);
