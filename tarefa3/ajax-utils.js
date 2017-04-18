642e242d9cc2894708d8c3ac64fde6f5af99e6c4
viniciusmiiller <vinicius.rebello@catolicasc.org.br>
2017-04-17 21:44:02 -0300

    Arrumando pastas

--- tarefa3/ajax-utils.js
+++ tarefa3/ajax-utils.js
@@ -1,66 +1,69 @@
 var $ajaxUtils = (function () {
 
-    // retornar os metodos expostos
-    return {
-        sendGetRequest: sendGetRequest
-    };
+// retornar os metodos expostos
+return {
+    sendGetRequest: sendGetRequest
+};
 
-    // Retorna um objeto HTTP Request
-    function RequestFactory() {
-        // se XMLHttpRequest for disponível
-        // retorne-o
-        if (window.XMLHttpRequest) {
-            return new XMLHttpRequest();
-            // senão, se ActiveXObject estiver disponível
-            // retorne-o e passe por parâmetro Microsoft.XMLHTTP
-        } else if (window.ActiveXObject) {
-            return ActiveXObject("Microsoft.XMLHTTP");
-            // caso nenhum desses seja disponível, emita um alerta e 
-            // retorne nulo
-        } else {
-            alert("Ajax não está disponível!");
-            return null;
-        }
+// Retorna um objeto HTTP Request
+function RequestFactory() {
+  // se XMLHttpRequest for disponível
+  // retorne-o
+  if (window.XMLHttpRequest) {
+    return new XMLHttpRequest();
+    // senão, se ActiveXObject estiver disponível
+    // retorne-o e passe por parâmetro Microsoft.XMLHTTP
+  } else if (window.ActiveXObject) {
+    return ActiveXObject("Microsoft.XMLHTTP");
+    // caso nenhum desses seja disponível, emita um alerta e 
+    // retorne nulo
+  } else {
+    alert("Ajax não está disponível!");
+    return null;
+  }
+}
+
+
+// Faz uma requisição Ajax GET para 'requestUrl'
+function sendGetRequest(requestUrl, responseHandler, 
+    isJsonResponse) {
+    // recebemos o objeto gerado dinâmicamente
+    // dependendo do navegador do usuário
+    var request = new RequestFactory();
+    // quando a requisição retornar
+    request.onreadystatechange = 
+      // execute a função anônima - 
+      // que vai executar handleResponse com os parâmetros 
+      function() { 
+        handleResponse(request, responseHandler, isJsonResponse); 
+      };
+    // Configure o método da requisição para GET
+    // e a URL passada por parâmetro
+    request.open("GET", requestUrl, true);
+    request.send(null); // só serve para POST
+}
+
+
+// Só chama a função 'responseHandler'
+// se a resposta estiver pronta
+// e não for um erro
+function handleResponse(request,
+                        responseHandler, isJsonResponse) {
+  // Se a resposta já estiver pronta e o status for 200 (OK)
+  // executa a função que foi passada por parâmetro
+  if ((request.readyState == 4) &&
+     (request.status == 200)) {
+    if (isJsonResponse === undefined) {
+      // default é true
+      isJsonResponse = true;
     }
-
-
-    // Faz uma requisição Ajax GET para 'requestUrl'
-    function sendGetRequest(requestUrl, responseHandler, isJsonResponse) {
-        // recebemos o objeto gerado dinâmicamente
-        // dependendo do navegador do usuário
-        var request = new RequestFactory();
-        // quando a requisição retornar
-        request.onreadystatechange = 
-        // execute a função anônima - 
-        // que vai executar handleResponse com os parâmetros 
-        function() { 
-            handleResponse(request, responseHandler, isJsonResponse); 
-        };
-        // Configure o método da requisição para GET
-        // e a URL passada por parâmetro
-        request.open("GET", requestUrl, true);
-        request.send(null); // só serve para POST
+    // Se receber um json, faça o parse
+    if (isJsonResponse) {
+      responseHandler(JSON.parse(request.responseText));
+    } else { 
+      responseHandler(request.responseText);
     }
-
-
-    // Só chama a função 'responseHandler'
-    // se a resposta estiver pronta
-    // e não for um erro
-    function handleResponse(request, responseHandler, isJsonResponse) {
-        // Se a resposta já estiver pronta e o status for 200 (OK)
-        // executa a função que foi passada por parâmetro
-        if ((request.readyState == 4) && (request.status == 200)) {
-            if (isJsonResponse === undefined) {
-                // default é true
-                isJsonResponse = true;
-            }
-            // Se receber um json, faça o parse
-            if (isJsonResponse) {
-                responseHandler(JSON.parse(request.responseText));
-            } else { 
-                responseHandler(request.responseText);
-            }
-        }
-    }
+  }
+}
 
 })();
--- tarefa3/script.js
+++ tarefa3/script.js
@@ -1,13 +1,8 @@
-// NOTE que este script foi o usado no exercício Ajax 4 e não vai funcionar
-// automaticamente com seu código. Você deve adaptá-lo, 
-// especialmente a função geraPaginaPrincipal, conforme
-// seu template HTML.
-
-var dataUrl = "menu.json";
-var itensHtml = "item-snippet.html";
+var dataUrl = "menu.json",
+    itensHtml = "item-snippet.html";
     
-// função facilitinsereHtmladora para inserir HTML em um elemento
-function (seletor, html) {
+// função facilitadora para inserir HTML em um elemento
+function insereHtml(seletor, html) {
   var elemento = document.querySelector(seletor);
   console.log(html);
   elemento.innerHTML = html;
@@ -22,31 +17,30 @@ function inserePropriedade(template, propName, propValue) {
   var propriedade = "{{" + propName + "}}";
   // substitui todas as ocorrências de propriedade por propValue
   // em template
-  template = template.replace(new RegExp(propriedade, "g"), propValue);
+  template = template.replace(new RegExp(propriedade, "g"),
+              propValue);
   return template;
 }
 
 // constroi a pagina, com os dados recebidos por parametro
 function constroiPagina(dados) {
-  $ajaxUtils.sendGetRequest(itensHtml, geraPaginaPrincipal, false); // não é um JSON
-}
-
-function geraPaginaPrincipal(dados) {
-    var htmlFinal = '<section class="row">'; // string que vai conter todo o HTML
-    // construimos os itens agora
+  var htmlFinal = '<section class="row">'; // string que vai conter todo o HTML
+  // construimos os itens agora
+  $ajaxUtils.sendGetRequest(itensHtml, function(itensHtml) {
     for (var i = 0, max = dados.length; i < max; i++) {
-          var html = itensHtml,
-            titulo = dados[i].titulo,
-            conteudo = dados[i].conteudo;
-          
-        html = inserePropriedade(html, "titulo", titulo);
-        html = inserePropriedade(html, "conteudo", conteudo);
-        htmlFinal += html;
+      var html = itensHtml,
+          titulo = dados[i].titulo,
+          conteudo = dados[i].Conteudo,
+        
+		  html = inserePropriedade(html, "titulo", titulo);
+		  html = inserePropriedade(html, "conteudo", conteudo);
+      
+      htmlFinal += html;
     }
     htmlFinal += '</section>';
     insereHtml("#content", htmlFinal);
+  }, false); // não é um JSON
 }
-
 // vamos construir o sendGetRequest:
 // definir a URL (dataUrl)
 // e o metodo constroiPagina
